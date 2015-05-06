ea5f2379d64b9738c514021a79f46d7af102797d
Scott Hardy <scott.the.hardy@gmail.com>
2015-05-05 23:22:30 -0700

    some changes

--- js/app.js
+++ js/app.js
@@ -2,16 +2,77 @@ angular.module('storeApp', [])
 
   .controller('orderController', function($scope) {
 
-  $scope.options = [
-    { label: '0', value: 0 },
-    { label: '1', value: 1 },
-    { label: '2', value: 2 },
-    { label: '3', value: 3 },
-    { label: '4', value: 4 },
-    { label: '5', value: 5 }
+  $scope.products = [
+    {
+      name: 'Original Token Stand',
+      id: 'original',
+      price: 4,
+      hide: false,
+      description: 'Description.',
+      options: [
+        {
+          name: 'Color',
+          options: [
+            { 'name': 'White (PLA)', id: 'white-pla', price: 0 },
+            { 'name': 'Black (PLA)', id: 'black-pla', price: 0 },
+            //{ 'name': 'Gray (HIPS)', id: 'gray-hips', price: 1 }
+          ],
+          selected: {}
+        },
+        {
+          name: 'Height',
+          options: [
+            { 'name': '1 cm', id: '1cm', price: 0 },
+            //{ 'name': '2 cm', id: '2cm', price: 1 }
+          ],
+          selected: {}
+        }
+      ]
+    },
+    {
+      name: 'Monitor Token Stand',
+      id: 'monitor',
+      price: 5,
+      hide: true,
+      description: 'Description.',
+      options: [
+        {
+          name: 'Color',
+          options: [
+            { 'name': 'White (PLA)', id: 'white-pla', price: 0 },
+            { 'name': 'Black (PLA)', id: 'black-pla', price: 0 },
+            //{ 'name': 'Gray (HIPS)', id: 'gray-pla', price: 1 }
+          ],
+          selected: {}
+        },
+        {
+          name: 'Monitor Type',
+          options: [
+            { 'name': 'Generic', id: 'generic', price: 0 },
+            //{ 'name': '27" Apple Thunderbolt Display', id: 'apple', price: 2 }
+          ],
+          selected: {}
+        }
+      ]
+    }
   ];
+
+  $scope.options = [0, 1, 2, 3, 4, 5];
  
-  $scope.myBed = $scope.options[0];
-  $scope.myNightstand = $scope.options[0];
-  $scope.myHammock = $scope.options[0];
+  $scope.totalPrice = function(product) {
+    var price = product.price;
+    product.options.forEach(function(option) {
+      price += option.selected.price;
+    });
+    return price;
+  };
+
+  $scope.itemNumber = function(product) {
+    var id = [];
+    id.push(product.id);
+    product.options.forEach(function(option) {
+      id.push(option.selected.id);
+    });
+    return id.join('-');
+  };
 });
