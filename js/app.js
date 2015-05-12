f5d346f2995b0665cabaea5561e07535422fe5a4
Scott Hardy <scott.the.hardy@gmail.com>
2015-05-11 20:02:04 -0700

    favicon and stuff

--- js/app.js
+++ js/app.js
@@ -2,77 +2,123 @@ angular.module('storeApp', [])
 
   .controller('orderController', function($scope) {
 
-  $scope.products = [
-    {
-      name: 'Original Token Stand',
-      id: 'original',
-      price: 4,
-      hide: false,
-      description: 'Description.',
-      options: [
-        {
-          name: 'Color',
-          options: [
-            { 'name': 'White (PLA)', id: 'white-pla', price: 0 },
-            { 'name': 'Black (PLA)', id: 'black-pla', price: 0 },
-            //{ 'name': 'Gray (HIPS)', id: 'gray-hips', price: 1 }
-          ],
-          selected: {}
-        },
-        {
-          name: 'Height',
-          options: [
-            { 'name': '1 cm', id: '1cm', price: 0 },
-            //{ 'name': '2 cm', id: '2cm', price: 1 }
-          ],
-          selected: {}
-        }
-      ]
-    },
-    {
-      name: 'Monitor Token Stand',
-      id: 'monitor',
-      price: 5,
-      hide: true,
-      description: 'Description.',
-      options: [
-        {
-          name: 'Color',
-          options: [
-            { 'name': 'White (PLA)', id: 'white-pla', price: 0 },
-            { 'name': 'Black (PLA)', id: 'black-pla', price: 0 },
-            //{ 'name': 'Gray (HIPS)', id: 'gray-pla', price: 1 }
-          ],
-          selected: {}
-        },
-        {
-          name: 'Monitor Type',
-          options: [
-            { 'name': 'Generic', id: 'generic', price: 0 },
-            //{ 'name': '27" Apple Thunderbolt Display', id: 'apple', price: 2 }
-          ],
-          selected: {}
-        }
-      ]
-    }
-  ];
+    $scope.cart = [];
+    $scope.products = [
+      {
+        name: 'Original Token Stand',
+        id: 'original',
+        price: 4,
+        hide: false,
+        description: 'Description.',
+        options: [
+          {
+            name: 'Color',
+            options: [
+              { 'name': 'White (PLA)', id: 'white-pla', price: 0 },
+              { 'name': 'Black (PLA)', id: 'black-pla', price: 0 },
+              { 'name': 'White (ABS)', id: 'white-abs', price: 0 },
+              { 'name': 'Black (ABS)', id: 'black-abs', price: 0 },
+              //{ 'name': 'Gray (HIPS)', id: 'gray-hips', price: 1 },
+            ],
+            selected: {}
+          },
+          {
+            name: 'Height',
+            options: [
+              { 'name': '1 cm', id: '1cm', price: 0 },
+              { 'name': '2 cm', id: '2cm', price: 1 },
+            ],
+            selected: {}
+          }
+        ]
+      },
+      {
+        name: 'Monitor Token Stand',
+        id: 'monitor',
+        price: 5,
+        hide: false,
+        description: 'Description.',
+        options: [
+          {
+            name: 'Color',
+            options: [
+              { 'name': 'White (PLA)', id: 'white-pla', price: 0 },
+              { 'name': 'Black (PLA)', id: 'black-pla', price: 0 },
+              { 'name': 'White (ABS)', id: 'white-abs', price: 0 },
+              { 'name': 'Black (ABS)', id: 'black-abs', price: 0 },
+              //{ 'name': 'Gray (HIPS)', id: 'gray-pla', price: 1 },
+            ],
+            selected: {}
+          },
+          {
+            name: 'Angle',
+            options: [
+              { 'name': '35°', id: '35', price: 0 },
+              { 'name': '45°', id: '45', price: 0 },
+              { 'name': '55°', id: '55', price: 0 },
+            ],
+            selected: {}
+          }
+        ]
+      }
+    ];
 
-  $scope.options = [0, 1, 2, 3, 4, 5];
- 
-  $scope.totalPrice = function(product) {
-    var price = product.price;
-    product.options.forEach(function(option) {
-      price += option.selected.price;
-    });
-    return price;
-  };
+    $scope.totalPrice = function(product) {
+      var price = product.price;
+      product.options.forEach(function(option) {
+        price += option.selected.price;
+      });
+      return price;
+    };
 
-  $scope.itemNumber = function(product) {
-    var id = [];
-    id.push(product.id);
-    product.options.forEach(function(option) {
-      id.push(option.selected.id);
+    $scope.productNumber = function(product) {
+      var id = [];
+      id.push(product.id);
+      product.options.forEach(function(option) {
+        id.push(option.selected.id);
+      });
+      return id.join('-');
+    };
+
+    $scope.addToCart = function(product) {
+      var productCopy = angular.copy(product, {});
+      $scope.cart.push(productCopy);
+    };
+
+    $scope.removeFromCart = function(index) {
+      $scope.cart.splice(index, 1);
+    };
+
+    $scope.cartPrice = function() {
+      var total = 0;
+      $scope.cart.forEach(function(product) {
+        total += $scope.totalPrice(product);
+      });
+      return total;
+    };
+
+    $scope.cartOptions = function(product) {
+      var options = [];
+      product.options.forEach(function(option) {
+        options.push(option.selected.name);
+      });
+      return options.join(' • ');
+    };
+
+    // save cart
+    $scope.$watch('cart.length', function(a, b) {
+      var cart = $scope.cart.map($scope.productNumber).join(',');
+      localStorage.setItem('cart', cart);
     });
-    return id.join('-');
-  };
-});
+
+    // load cart
+    var loadCart = function() {
+      var cart = localStorage.getItem('cart');
+      if (cart) {
+        cart.split(',').forEach(function(id) {
+        });
+      }
+    };
+    loadCart();
+
+  });
